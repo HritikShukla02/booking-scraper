cefce1ae84ab97d92f72cdb9ec0d719ab4cfd73e
HritikShukla02 <shritik83385@gmail.com>
2024-06-24 19:18:37 +0530

    added main with error handelling 1

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time
 
 URL = 'https://www.booking.com/'
-PAUSE_TIME = 0.5
+PAUSE_TIME = 1
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
-wait = WebDriverWait(driver, 5)
+wait = WebDriverWait(driver, 10)
 
 driver.get(URL)
-try:
+try: 
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[aria-label="Dismiss sign in information."]')))
     popup_close = driver.find_element(By.CSS_SELECTOR,value='button[aria-label="Dismiss sign in information."]')
     popup_close.click()
-finally:
-    pass
+except TimeoutException as ex:
+    print("Exception has been thrown. " + str(ex))
+
+
 
 dest = driver.find_element(By.CSS_SELECTOR, value='input[name="ss"]')
 dest.send_keys('Jaipur', Keys.ENTER)
@@ -28,21 +31,23 @@ time.sleep(PAUSE_TIME)
 # wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.b90aaa8bb3[data-testid="searchbox-dates-container"]')))
 
 
-date_picker = driver.find_element(By.CSS_SELECTOR, value='button[data-testid="date-display-field-start"]')
+# date_picker = driver.find_element(By.CSS_SELECTOR, value='div.baca574774')
 
-date_picker.click()
+# date_picker.click()
 '#calendar-searchboxdatepicker >  table > tbody > tr > td > span[data-date="2024-06-27"]'
 
 time.sleep(PAUSE_TIME)
+try:
+    check_in = driver.find_element(By.CSS_SELECTOR, value='div.e8314cc830 > span[data-date="2024-06-27"]')
+    check_in.click()
 
-check_in = driver.find_element(By.CSS_SELECTOR, value='table > tbody > tr > td > span[data-date="2024-06-27"]')
-check_in.click()
-
-# time.sleep(PAUSE_TIME)
+    # time.sleep(PAUSE_TIME)
 
 
-check_out = driver.find_element(By.CSS_SELECTOR, value='table > tbody > tr > td > span[data-date="2024-07-27"]')
-check_out.click()
+    check_out = driver.find_element(By.CSS_SELECTOR, value='div.e8314cc830 > span[data-date="2024-07-27"]')
+    check_out.click()
+except NoSuchElementException as ex:
+    print("Exception has been thrown. " + str(ex))
 
 search = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]') 
 search.click()
@@ -65,11 +70,15 @@ while True:
         break
     last_height = new_height
 
+    wait.until(EC.element_to_be_clickable, 'span.ad82e69f7d')
     try:
-        load = driver.find_element(By.CSS_SELECTOR, value='span.ad82e69f7d')
+        load = driver.find_element(By.CSS_SELECTOR, value='button.bf33709ee1.a190bb5f27.b9d0a689f2.bb5314095f.b81c794d25.da4da790cd')
         load.click()
-    finally:
-        pass
+    except NoSuchElementException as ex:
+        print("Error message: " + str(ex))
+
+
+
 
 
 
