5c47da121ff44ad1451352bac2f8fc89e8a346fe
Hritik Shukla <108985939+HritikShukla02@users.noreply.github.com>
2024-07-15 14:20:34 +0530

    Update pipelines.py

--- booking/booking/pipelines.py
+++ booking/booking/pipelines.py
@@ -10,4 +10,29 @@ from itemadapter import ItemAdapter
 
 class BookingPipeline:
     def process_item(self, item, spider):
+        
+        adaptor = ItemAdapter(item)
+
+        fields = adaptor.field_names()
+
+        for field in fields:
+            if field == "price":
+                value = adaptor.get(field)
+                adaptor[field] = value.replace(r"\xa", " ")
+            if field == "ratings":
+                value = adaptor.get(field)
+                if value:
+                    adaptor[field] = value.split(' ')[1]
+                else:
+                    adaptor[field] = 0
+
+            if field == "reviews":
+                value = adaptor.get(field)
+                if value:
+                    adaptor[field] = value.split(' ')[0]
+                else:
+                    adaptor[field] = 0
+
         return item
+
+
